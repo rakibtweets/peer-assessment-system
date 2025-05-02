2235fd483d084b772d092f299fd2a5ab4e6130f1
Rakib Hasan <rakibhasanroky0@gmail.com>
2025-05-03 03:30:45 +0600

    fix: update JWT_SECRET environment variable reference in auth-utils

--- lib/auth-utils.ts
+++ lib/auth-utils.ts
@@ -2,7 +2,7 @@
 //auth-utils.ts
 import jwt from 'jsonwebtoken';
 
-const SECRET_KEY = process.env.NEXT_PUBLIC_JWT_SECRET as string;
+const SECRET_KEY = process.env.JWT_SECRET as string;
 
 if (!SECRET_KEY) {
   throw new Error('JWT_SECRET environment variable is not defined');
