0b7ebbbeadd71a30029bb8b65e0cccedce1ba7c6
Rakib Hasan <rakibhasanroky0@gmail.com>
2025-05-03 22:41:02 +0600

    refactor: update layout structure for Home and Layout components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="relative">
+    <main className="relative w-full">
       <Navbar />
-      <div className="flex">
+      <div>
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-6xl">{children}</div>
         </section>
